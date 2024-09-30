c1ce1200196c9785d378b967908c1d257f040dd6
YadavYashvant <yashvantyadav855@gmail.com>
2024-09-30 23:47:33 +0530

    Setting up navigation

--- app/src/main/java/com/yashvant/synovate_app/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/yashvant/synovate_app/ui/screens/HomeScreen.kt
@@ -1,6 +1,7 @@
 package com.yashvant.synovate_app.ui.screens
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
@@ -8,8 +9,12 @@ import androidx.navigation.NavController
 @Composable
 fun HomeScreen(navController: NavController) {
 
-    Column {
-        Text(text = "Home Screen")
+    LazyColumn {
+        item {
+            Column {
+                Text("Hello, World!")
+            }
+        }
     }
 
 }
