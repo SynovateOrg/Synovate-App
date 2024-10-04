cca3d0a6a134baadb16cbebfb5afc29e58e3fe20
YadavYashvant <yashvantyadav855@gmail.com>
2024-10-04 23:41:40 +0530

    Creating task screen

--- app/src/main/java/com/yashvant/synovate_app/MainActivity.kt
+++ app/src/main/java/com/yashvant/synovate_app/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -35,10 +36,16 @@ class MainActivity : ComponentActivity() {
                                 HomeScreen(navController)
                             }
 
+                            composable("tasks") {
+                                TaskScreen(navController)
+                            }
+
                         }
                     }
                 )
             }
         }
     }
+
+
 }
---
+++ app/src/main/java/com/yashvant/synovate_app/ui/screens/TaskScreen.kt
@@ -0,0 +1,9 @@
+package com.yashvant.synovate_app.ui.screens
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun TaskScreen(navController: NavHostController) {
+
+}
